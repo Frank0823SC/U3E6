45779e7a61b9b26d1251deed91eb1507c75c877f
Frank0823SC <71744464+Frank0823SC@users.noreply.github.com>
2020-12-01 18:24:42 -0600

    Update Main.java

--- EliminarEnListasEnlazadas/Main.java
+++ EliminarEnListasEnlazadas/Main.java
@@ -1,1 +1,38 @@
-
+package EliminarEnListasEnlazadas;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[]args){
+        
+        Scanner leer = new Scanner(System.in);
+        boolean continuar = true;
+        int opcion;        
+        ListasEnlazada yo = new ListasEnlazada();
+        
+        while(continuar){
+            System.out.println("");
+            System.out.println("*********************************************");
+            System.out.println("********************Menú*********************");
+            System.out.println("****1.- Insertar Posición                ****");
+            System.out.println("****2.- Eliminar                         ****");
+            System.out.println("****3.- Mostrar                          ****");            
+            System.out.println("****4.- Salir                            ****");
+            System.out.println("*********************************************");
+            System.out.println("*********************************************");            
+            opcion = leer.nextInt();
+            
+            switch(opcion){
+                case 1 -> {                    
+                    
+                }      
+                case 2 ->{                    
+                    yo.Eliminar();
+                }
+                case 3 -> {                    
+                    yo.Mostrar();
+                }
+                case 4 -> continuar = false;
+                default -> System.out.println("Opción incorrecta.....Vuelve a intentarlo");                                                  
+            }
+        }
+    }
+}
